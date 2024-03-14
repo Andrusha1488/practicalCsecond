4d5861349899c7e0934403bc6c2153765276a063
Andrusha1488 <145493233+Andrusha1488@users.noreply.github.com>
2024-03-14 23:00:07 +0300

    3 практос

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,98 +1,164 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Newtonsoft.Json;
+using Microsoft.Win32;
 
-namespace practicheskaya2
+namespace AudioPlayerApp
 {
-    public class Note
+    public partial class MainWindow : Window
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime Date { get; set; }
-    }
+        private List<string> _playlist;
+        private int _currentTrackIndex;
+        private bool _isPlaying;
+        private bool _isRepeating;
+        private bool _isShuffling;
+        private Thread _positionThread;
+        private Thread _timeDisplayThread;
 
-    public class DataSerializer
-    {
-        public void Serialize<T>(List<T> data, string filePath)
+        public MainWindow()
         {
-            string jsonData = JsonConvert.SerializeObject(data);
-            File.WriteAllText(filePath, jsonData);
+            InitializeComponent();
+            _playlist = new List<string>();
+            _isPlaying = false;
+            _isRepeating = false;
+            _isShuffling = false;
         }
 
-        public List<T> Deserialize<T>(string filePath)
+        private void OpenFolderButton_Click(object sender, RoutedEventArgs e)
         {
-            if (File.Exists(filePath))
+            var dialog = new CommonOpenFileDialog { IsFolderPicker = true };
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                string jsonData = File.ReadAllText(filePath);
-                return JsonConvert.DeserializeObject<List<T>>(jsonData);
+                string[] musicFiles = Directory.GetFiles(dialog.FileName, "*.*", SearchOption.AllDirectories)
+                    .Where(file => file.EndsWith(".mp3") || file.EndsWith(".m4a") || file.EndsWith(".wav"))
+                    .ToArray();
+
+                if (musicFiles.Length > 0)
+                {
+                    _playlist.Clear();
+                    _playlist.AddRange(musicFiles);
+                    _currentTrackIndex = 0;
+                    StartPlayback(_playlist[_currentTrackIndex]);
+                }
+            }
+        }
+
+        private void StartPlayback(string track)
+        {
+            mediaElement.Source = new Uri(track);
+            mediaElement.Play();
+            _isPlaying = true;
+            // Start threads for position slider and time display
+            _positionThread = new Thread(UpdatePositionSlider);
+            _positionThread.Start();
+            _timeDisplayThread = new Thread(UpdateTimeDisplay);
+            _timeDisplayThread.Start();
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isPlaying)
+            {
+                mediaElement.Pause();
+                _isPlaying = false;
             }
             else
             {
-                return new List<T>();
+                mediaElement.Play();
+                _isPlaying = true;
             }
         }
-    }
 
-    public class DailyPlannerViewModel
-    {
-        private List<Note> notes;
-        private DataSerializer serializer;
-        private string filePath = "notes.json";
-
-        public DailyPlannerViewModel()
+        private void NextTrackButton_Click(object sender, RoutedEventArgs e)
         {
-            serializer = new DataSerializer();
-            LoadNotes();
-        }
-
-        public List<Note> Notes
-        {
-            get { return notes; }
-            set { notes = value; }
-        }
-
-        private void LoadNotes()
-        {
-            notes = serializer.Deserialize<Note>(filePath);
-        }
-
-        public void SaveNotes()
-        {
-            serializer.Serialize(notes, filePath);
-        }
-
-        public void AddOrUpdateNote(Note note)
-        {
-            int index = notes.FindIndex(n => n.Date == note.Date);
-            if (index != -1)
+            if (_isShuffling)
             {
-                notes[index] = note;
+                ShufflePlaylist();
             }
             else
             {
-                notes.Add(note);
+                _currentTrackIndex = (_currentTrackIndex + 1) % _playlist.Count;
             }
-            SaveNotes();
+            StartPlayback(_playlist[_currentTrackIndex]);
         }
 
-        public void DeleteNote(Note note)
+        private void PreviousTrackButton_Click(object sender, RoutedEventArgs e)
         {
-            notes.Remove(note);
-            SaveNotes();
+            _currentTrackIndex = (_currentTrackIndex - 1 + _playlist.Count) % _playlist.Count;
+            StartPlayback(_playlist[_currentTrackIndex]);
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            _isRepeating = !_isRepeating;
+        }
+
+        private void ShuffleButton_Click(object sender, RoutedEventArgs e)
+        {
+            _isShuffling = !_isShuffling;
+            if (_isShuffling)
+            {
+                ShufflePlaylist();
+            }
+            else
+            {
+                _playlist.Sort();
+            }
+        }
+
+        private void ShufflePlaylist()
+        {
+            Random rng = new Random();
+            int n = _playlist.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                string value = _playlist[k];
+                _playlist[k] = _playlist[n];
+                _playlist[n] = value;
+            }
+        }
+
+        private void UpdatePositionSlider()
+        {
+            while (mediaElement.NaturalDuration.HasTimeSpan && _isPlaying)
+            {
+                TimeSpan duration = mediaElement.NaturalDuration.TimeSpan;
+                double totalSeconds = duration.TotalSeconds;
+                double currentPosition = mediaElement.Position.TotalSeconds;
+                Dispatcher.Invoke(() => positionSlider.Value = (currentPosition / totalSeconds) * 100);
+                Thread.Sleep(500);
+            }
+        }
+
+        private void UpdateTimeDisplay()
+        {
+            while (mediaElement.NaturalDuration.HasTimeSpan && _isPlaying)
+            {
+                TimeSpan duration = mediaElement.NaturalDuration.TimeSpan;
+                TimeSpan remainingTime = duration - mediaElement.Position;
+                Dispatcher.Invoke(() =>
+                {
+                    currentTimeLabel.Content = mediaElement.Position.ToString(@"mm\:ss");
+                    remainingTimeLabel.Content = remainingTime.ToString(@"mm\:ss");
+                });
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            HistoryWindow historyWindow = new HistoryWindow(_playlist);
+            if (historyWindow.ShowDialog() == true)
+            {
+                _currentTrackIndex = historyWindow.SelectedTrackIndex;
+                StartPlayback(_playlist[_currentTrackIndex]);
+            }
         }
     }
 }
